c4c7547721ef3c64bb74c69e9e727d4d917b1dad
online <ankitsingh5991111@gmail.com>
2020-08-10 10:20:44 +0530

    Updates

---
+++ precache-manifest.e45e023b6ecfca833193cbc79e34e2d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07bb0f5649a89628dcb45d39b17d846a",
+    "url": "/spinny/index.html"
+  },
+  {
+    "revision": "a7f7a6daaa5257fe1565",
+    "url": "/spinny/static/css/main.ad04c162.chunk.css"
+  },
+  {
+    "revision": "e16f3ad6ae93bbca5f21",
+    "url": "/spinny/static/js/2.9bc6a629.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/spinny/static/js/2.9bc6a629.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7f7a6daaa5257fe1565",
+    "url": "/spinny/static/js/main.f29993de.chunk.js"
+  },
+  {
+    "revision": "48645aaf39953c9f96d3",
+    "url": "/spinny/static/js/runtime-main.df9cfd79.js"
+  }
+]);
